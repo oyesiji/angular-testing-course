90170013c8b8fa9469abe248e51fdb45adf4b5a9
Jubril Oyesiji <oyesiji@gmail.com>
2025-06-10 17:07:31 -0400

    learning test

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,17 +1,36 @@
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 describe('CalculatorService', () => {
+    let calculator: CalculatorService;
+    let loggerSpy: any;
+    
+    beforeEach(()=>{
+        console.log("Calling each");
+        loggerSpy = jasmine.createSpyObj('LoggerService',["log"]);
+        TestBed.configureTestingModule({
+            providers : [
+                CalculatorService,
+                {provide: LoggerService, useValue:loggerSpy}
+            ]
+        });
+
+
+        calculator = TestBed.inject(CalculatorService);
+    });
+
+
     it('should add two numbers', ()=>{
-       const logger = jasmine.createSpyObj('LoggerService',["log"]);
-       const calculator = new CalculatorService(logger);
+       console.log("Calling add");
        const result = calculator.add(2,2);
        expect(result).toBe(4);
-       expect(logger.log).toHaveBeenCalledTimes(1);
+       expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     });
 
 
     it('should substract two numbers', ()=>{
+        console.log("Calling substract");
         const calculator = new CalculatorService(new LoggerService());
         const result = calculator.subtract(10,5);
         expect(result).toBe(5);
