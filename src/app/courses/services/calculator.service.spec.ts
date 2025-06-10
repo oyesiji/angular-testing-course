63cf92ff15af9986910acf0ee60e08fa4a245377
Jubril Siji <oyesiji@gmail.com>
2025-06-10 05:26:39 -0400

    creating test

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,19 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
+describe('CalculatorService', () => {
+    it('should add two numbers', ()=>{
+
+       const calculator = new CalculatorService(new LoggerService());
+       const result = calculator.add(2,2);
+       expect(result).toBe(4);
+    });
+
+
+    it('should substract two numbers', ()=>{
+        const calculator = new CalculatorService(new LoggerService());
+        const result = calculator.subtract(10,5);
+        expect(result).toBe(5);
+    });
+
+});
